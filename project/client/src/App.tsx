37aa51f8df99e3c2284b5c23a696738bdd891285
ben izak <93186925+BenIzak@users.noreply.github.com>
2024-04-15 10:41:24 +0200

    feat : add react query

--- project/client/src/App.tsx
+++ project/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Provider } from 'react-redux'
 import { store } from '@redux/store'
@@ -39,6 +40,7 @@ export default function App() {
                         </Suspense>
                     </main>
                 </Router>
+                <ReactQueryDevtools initialIsOpen={false} />
             </QueryClientProvider>
         </Provider>
     )
