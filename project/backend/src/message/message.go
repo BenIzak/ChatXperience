7691e5c1dc495ae99a4a23f677d7054dc3978b65
Watsuk <pierre.allepro@gmail.com>
2024-04-17 06:36:41 +0200

    add user informations with messages

--- project/backend/src/message/message.go
+++ project/backend/src/message/message.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/BenIzak/ChatXperience/project/src/entity"
+	"github.com/BenIzak/ChatXperience/project/src/user"
 )
 
 func CreateMessage(db *sql.DB, userID int, groupID int, content string) (*entity.Message, error) {
@@ -23,37 +24,47 @@ func CreateMessage(db *sql.DB, userID int, groupID int, content string) (*entity
 	return message, nil
 }
 
-func GetMessagesByGroupID(db *sql.DB, groupID int) ([]*entity.Message, error) {
+type messageData struct {
+	Message entity.Message
+	User    entity.User
+}
+
+func GetMessagesByGroupID(db *sql.DB, groupID int) ([]*messageData, error) {
 	rows, err := db.Query("SELECT * FROM messages WHERE group_id = ?", groupID)
+	if err == sql.ErrNoRows {
+		log.Printf("no message in this group")
+		return nil, nil
+	}
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	messages := []*entity.Message{}
+	messages := []*messageData{}
 	for rows.Next() {
-		message := &entity.Message{}
+		message := &messageData{}
 		var createdAt, updated_at []uint8
-		err := rows.Scan(&message.ID, &message.UserID, &message.GroupID, &message.Content, &createdAt, &updated_at)
+		err := rows.Scan(&message.Message.ID, &message.Message.UserID, &message.Message.GroupID, &message.Message.Content, &createdAt, &updated_at)
 		if err != nil {
 			return nil, err
 		}
 
 		createdAtString := string(createdAt)
-		message.CreatedAt, err = time.Parse("2006-01-02 15:04:05", createdAtString)
+		message.Message.CreatedAt, err = time.Parse("2006-01-02 15:04:05", createdAtString)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		updatedAtString := string(updated_at)
-		message.UpdatedAt, err = time.Parse("2006-01-02 15:04:05", updatedAtString)
+		message.Message.UpdatedAt, err = time.Parse("2006-01-02 15:04:05", updatedAtString)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		message.User, err = user.GetUserByID(db, message.Message.UserID)
+		log.Println(message)
 		messages = append(messages, message)
 	}
-
 	return messages, nil
 }
 
